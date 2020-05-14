d831802eaa0a9473831cb719279824afda2fd5cb
supreeth <supreeth97tamvada@gmail.com>
2020-05-13 22:04:49 -0500

    first commit

---
+++ music.js
@@ -0,0 +1,18 @@
+// Put your Last.fm API key here
+var api_key = "";
+
+function sendRequest () {
+    var xhr = new XMLHttpRequest();
+    var method = "artist.getinfo";
+    var artist = encodeURI(document.getElementById("form-input").value);
+    xhr.open("GET", "proxy.php?method="+method+"&artist="+artist+"&api_key="+api_key+"&format=json", true);
+    xhr.setRequestHeader("Accept","application/json");
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            var json = JSON.parse(this.responseText);
+            var str = JSON.stringify(json,undefined,2);
+            document.getElementById("output").innerHTML = "<pre>" + str + "</pre>";
+        }
+    };
+    xhr.send(null);
+}
